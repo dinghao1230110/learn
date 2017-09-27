70979594e0123933f777771b8ba849406235358f
mt <oc.mt@hotmail.com>
2017-09-27 23:41:32 +0800

    日志

--- src/main/java/org/hao/learn/aspect/FunctionAspect.java
+++ src/main/java/org/hao/learn/aspect/FunctionAspect.java
@@ -1,11 +1,13 @@
 package org.hao.learn.aspect;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.hao.learn.annotate.Function;
 import org.hao.learn.api.FunctionDataBaseService;
+import org.hao.learn.api.LogService;
 import org.hao.learn.exception.AuthenticationException;
 import org.hao.learn.person.domain.FunctionInfo;
 import org.slf4j.Logger;
@@ -27,6 +29,8 @@ public class FunctionAspect {
     HttpSession                           httpSession;
     @Autowired
     FunctionDataBaseService<FunctionInfo> functionService;
+    @Autowired
+    LogService                            logService;
 
 
     /**
@@ -35,12 +39,14 @@ public class FunctionAspect {
      * @param joinPoint
      */
     @Before("execution(* org.hao.learn..*Controller.*(..))")
-    public void valid(JoinPoint joinPoint) throws NoSuchMethodException {
+    public void valid(JoinPoint joinPoint) throws NoSuchMethodException, JsonProcessingException {
         //region 获取拦截的方法
         MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
         Method          targetMethod    = methodSignature.getMethod();
         //从 session 缓存中取出当前登录用户持有的功能
         Object functionsObj = httpSession.getAttribute("functions");
+        //获取方法参数的值
+        Object[] args = joinPoint.getArgs();
         //转换 object 为 Map<Long, FunctionInfo>
         Map<Long, FunctionInfo> functions = (Map<Long, FunctionInfo>) functionsObj;
 
@@ -51,7 +57,9 @@ public class FunctionAspect {
                 FunctionInfo functionInfo = functionService.queryFunctionById(function.value());
 
                 if (!functions.containsKey(function.value())) {
-                    throw new AuthenticationException("您没有访问 " + functionInfo.getName() + " 的权限");
+                    String errorMsg = "您没有访问 " + functionInfo.getName() + " 的权限";
+                    logService.addFailureLog("鉴权", args, errorMsg);
+                    throw new AuthenticationException(errorMsg);
                 }
             }
         }
